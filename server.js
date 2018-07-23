971a9d9b81b3801bab568eb5735a2555a80e4b72
misia <monika.flisowska@gmail.com>
2018-07-23 14:24:48 +0100

    created handler function

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var http = require('http');
 
-var server = http.createServer();
+var message = "It's my birthday!";
+
+function handler(request, response) {
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(message);
+    response.end();
+}
+
+var server = http.createServer(handler);
 
 server.listen(3000, function() {
     console.log("Server is listening op port 3000. Ready to accept requests!");
